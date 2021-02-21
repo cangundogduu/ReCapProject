ee294ea1c13c92890ff8a902816914d63ba7bbb5
Can Gündoğdu <gundogdu.can@hotmail.com>
2021-02-22 00:04:47 +0300

    12.Ders 2.Ödev

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,9 +1,12 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 
@@ -36,10 +39,9 @@ namespace Business.Concrete
 
         public IResult Add(Car car)
         {
-            if (car.CarName.Length < 2)
-            {
-                return new ErrorDataResult<Car>(Messages.CarNameInvalid);
-            }
+            ValidationTool.Validate(new CarValidator(), car);
+
+
             _carDal.Add(car);
             return new SuccessDataResult<Car>(Messages.CarAdded);
         }
